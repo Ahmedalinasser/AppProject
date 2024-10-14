94ca45b94a9ff2d36c3f3275431f6d842df9da3e
Ahmed Ali <Ahmed Ali@DESKTOP-8T3J0SK>
2024-10-14 17:40:50 +0300

    Add project files.

---
+++ Demo.DAL/Context/App3TierArch.cs
@@ -0,0 +1,25 @@
+﻿using Demo.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.DAL.Context
+{
+    public class App3TierArch : DbContext
+    {
+        
+        public App3TierArch(DbContextOptions<App3TierArch> options ) : base(options)
+        {
+        }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //    =>optionsBuilder.UseSqlServer("Server = .; Database = App3TierArc; Trusted_Connection = true; MultipleActiveResultSets = true");
+        
+
+        public DbSet<Department> Departments { get; set; }   
+
+    }
+}
